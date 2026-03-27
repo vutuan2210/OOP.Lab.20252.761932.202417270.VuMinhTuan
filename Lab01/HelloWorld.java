6bdc206c814df9f5f167363cfc31d4485426038b
vutuan2210 <vuminhtuanv006@gmail.com>
2026-03-27 10:48:25 +0700

    test

---
+++ Lab01/HelloWorld.java
@@ -0,0 +1,10 @@
+//Example 1: Hello World
+//Text-printing program
+public class HelloWorld {
+
+        public static void main(String args[]){
+            System.out.println("Xin chao \n cac ban!");
+            System.out.println("Hello World!");
+
+        } // end of method again
+}
