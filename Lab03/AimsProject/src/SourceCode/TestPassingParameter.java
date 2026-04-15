4f13e8297bd3ae9f0ce99d663fe76fcb44a228bf
vutuan2210 <vuminhtuanv006@gmail.com>
2026-04-16 00:33:20 +0700

    Implement method overloading for Cart in lab03

---
+++ Lab03/AimsProject/src/SourceCode/TestPassingParameter.java
@@ -0,0 +1,33 @@
+package SourceCode;
+
+public class TestPassingParameter {
+
+    public static void main(String[] args) {
+
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc(
+                "Jungle", "Adventure", "Someone", 120, 10.0f);
+
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc(
+                "Cinderella", "Animation", "Disney", 90, 12.0f);
+
+        swap(jungleDVD, cinderellaDVD);
+
+        System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
+        System.out.println("Cinderella DVD title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+
+        System.out.println("After changing title:");
+        System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        DigitalVideoDisc temp = dvd1;
+        dvd1 = dvd2;
+        dvd2 = temp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        dvd.setTitle(title);
+    }
+}
